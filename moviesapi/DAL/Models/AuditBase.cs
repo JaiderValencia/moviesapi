dc2bb65e8708d6af0187706409a79e8d3d841cd2
JaiderValencia <janvame05@gmail.com>
2025-11-24 12:35:42 -0500

    refactor: Make UpdatedAt field nullable in AuditBase

--- moviesapi/DAL/Models/AuditBase.cs
+++ moviesapi/DAL/Models/AuditBase.cs
@@ -8,6 +8,6 @@ namespace moviesapi.DAL.Models
         public virtual int Id { get; set; }
 
         public virtual DateTime CreatedAt { get; set; }
-        public virtual DateTime UpdatedAt { get; set; }
+        public virtual DateTime? UpdatedAt { get; set; }
     }
 }
